7129676e2795bd9d070cd9db171909ea8fd1c6c8
suneethitp <suneethitp14@gmail.com>
2025-12-26 13:01:39 +0530

    fix:add packing details in SO and item

--- cit_exim/hooks.py
+++ cit_exim/hooks.py
@@ -264,7 +264,7 @@ fixtures=[
         ["name", "in", [
            "Sales Order-main-field_order","Sales Order-shipping_terms-label","Sales Order-po_no-label","Supplier-main-field_order","Sales Order-customer-label","Address-is_shipping_address-depends_on",
            "Address-main-field_order","Journal Entry-voucher_type-options","Contract Term-custom_document_no","Item-main-field_order","Sales Invoice-main-field_order","Address-main-field_order","Item-main-field_order",
-           "Sales Invoice-main-field_order","Purchase Order-main-field_order","Sales Order-main-field_order","Purchase Invoice-main-field_order"
+           "Sales Invoice-main-field_order","Purchase Order-main-field_order","Sales Order-main-field_order","Purchase Invoice-main-field_order","Sales Order-main-field_order","Purchase Order-main-field_order","Item-main-field_order"
         ]]
     ]
     },
--- cit_exim/public/js/doctype_js/sales_order.js
+++ cit_exim/public/js/doctype_js/sales_order.js
@@ -885,3 +885,43 @@ frappe.ui.form.on("Sales Order", {
         );
     }
 });
+
+
+//FETCH PACKING DETAILS FROM ITEM MASTER
+frappe.ui.form.on("Sales Order Item", {
+    item_code(frm, cdt, cdn) {
+
+        let row = locals[cdt][cdn];
+
+        // If item removed, clear parent fields
+        if (!row.item_code) {
+            frm.set_value("custom_packing_template", "");
+            frm.set_value("custom_packing_details", "");
+            return;
+        }
+
+        // Fetch values from Item master
+        frappe.db.get_value(
+            "Item",
+            row.item_code,
+            [
+                "custom_name_of_packing",
+                "custom_packing_detailsfor_sales_contract"
+            ]
+        ).then(r => {
+            if (r && r.message) {
+
+                // Set values in Sales Contract (parent)
+                frm.set_value(
+                    "custom_packing_template",
+                    r.message.custom_name_of_packing
+                );
+
+                frm.set_value(
+                    "custom_packing_details",
+                    r.message.custom_packing_detailsfor_sales_contract
+                );
+            }
+        });
+    }
+});
