d40d8fa5b23878042f1f264d5c722167cfa1e741
suneethitp <suneethitp14@gmail.com>
2026-02-20 16:13:25 +0530

    brc create

--- cit_exim/cit_exim/doc_events/sales_invoice.py
+++ cit_exim/cit_exim/doc_events/sales_invoice.py
@@ -1320,17 +1320,17 @@ def create_jv(self):
                     self.db_set("meis_jv", meis_jv.name)
 
 
-# def create_brc(self):
-# 	if frappe.db.get_value('Address', self.customer_address, 'country') != "India" and frappe.db.exists("DocType", "BRC Management"):
-# 		brc = frappe.new_doc("BRC Management")
-# 		brc.invoice_no = self.name
-# 		if not self.is_return and self.shipping_bill_number and self.shipping_bill_date and self.rounded_total:
-# 			brc.append("shipping_bill_details", {
-# 				"shipping_bill": self.shipping_bill_number,
-# 				"shipping_date": self.shipping_bill_date,
-# 				"shipping_bill_amount": self.rounded_total
-# 			})
-# 		brc.save(ignore_permissions=True)
+def create_brc(self):
+	if frappe.db.get_value('Address', self.customer_address, 'country') != "India" and frappe.db.exists("DocType", "BRC Management"):
+		brc = frappe.new_doc("BRC Management")
+		brc.invoice_no = self.name
+		if not self.is_return and self.shipping_bill_number and self.shipping_bill_date and self.rounded_total:
+			brc.append("shipping_bill_details", {
+				"shipping_bill": self.shipping_bill_number,
+				"shipping_date": self.shipping_bill_date,
+				"shipping_bill_amount": self.rounded_total
+			})
+		brc.save(ignore_permissions=True)
 
 
 def cancel_export_lic(self):
